7dfd005e315068b61df66b4f6efdd6f649c3f24e
Fawad <fawad.haider@gmail.com>
2022-05-16 17:55:53 -0700

    adding theta unit test

--- black_scholes_test.py
+++ black_scholes_test.py
@@ -110,67 +110,67 @@ def get_random_test_input():
 #             got_put, exp_put))
 
 
-@pytest.mark.asyncio
-async def test_randomized_black_scholes_theta():
-    # Create a new Starknet class that simulates the StarkNet system.
-    starknet = await Starknet.empty()
+# @pytest.mark.asyncio
+# async def test_randomized_black_scholes_theta():
+#     # Create a new Starknet class that simulates the StarkNet system.
+#     starknet = await Starknet.empty()
 
-    # Deploy the contract.
-    contract_def = compile_starknet_files(files=[CONTRACT_FILE],
-                                          disable_hint_validation=True)
-    contract = await starknet.deploy(
-        contract_def=contract_def,
-    )
+#     # Deploy the contract.
+#     contract_def = compile_starknet_files(files=[CONTRACT_FILE],
+#                                           disable_hint_validation=True)
+#     contract = await starknet.deploy(
+#         contract_def=contract_def,
+#     )
 
-    # Number of random tests to run.
-    ITERATIONS = 10
+#     # Number of random tests to run.
+#     ITERATIONS = 10
 
-    # List of float tuple (t_annualised, volatility, spot, strike, rate).
-    test_inputs = []
+#     # List of float tuple (t_annualised, volatility, spot, strike, rate).
+#     test_inputs = []
 
-    # Query the contract for options prices.
-    tasks = []
-    for i in range(ITERATIONS):
-        test_input = get_random_test_input()
-        test_inputs.append(test_input)
-        tasks.append(contract.theta(
-            t_annualised=get_precise(test_input[0]),
-            volatility=get_precise(test_input[1]),
-            spot=get_precise(test_input[2]),
-            strike=get_precise(test_input[3]),
-            rate=get_precise(test_input[4])).call())
+#     # Query the contract for options prices.
+#     tasks = []
+#     for i in range(ITERATIONS):
+#         test_input = get_random_test_input()
+#         test_inputs.append(test_input)
+#         tasks.append(contract.theta(
+#             t_annualised=get_precise(test_input[0]),
+#             volatility=get_precise(test_input[1]),
+#             spot=get_precise(test_input[2]),
+#             strike=get_precise(test_input[3]),
+#             rate=get_precise(test_input[4])).call())
 
-    # Compare call and put prices with the python black scholes library.
-    print()
-    execution_infos = await asyncio.gather(*tasks)
-    for i, execution_info in enumerate(execution_infos):
-        (got_call, got_put) = (get_lift(execution_info.result.call_theta)/UNIT,
-                               get_lift(execution_info.result.put_theta)/UNIT)
+#     # Compare call and put prices with the python black scholes library.
+#     print()
+#     execution_infos = await asyncio.gather(*tasks)
+#     for i, execution_info in enumerate(execution_infos):
+#         (got_call, got_put) = (get_lift(execution_info.result.call_theta)/UNIT,
+#                                get_lift(execution_info.result.put_theta)/UNIT)
 
-        (exp_call, exp_put) = (
-            theta('c', test_inputs[i][2], test_inputs[i][3],
-                  test_inputs[i][0], test_inputs[i][4],
-                  test_inputs[i][1]),
-            theta('p', test_inputs[i][2], test_inputs[i][3],
-                  test_inputs[i][0], test_inputs[i][4],
-                  test_inputs[i][1]))
+#         (exp_call, exp_put) = (
+#             theta('c', test_inputs[i][2], test_inputs[i][3],
+#                   test_inputs[i][0], test_inputs[i][4],
+#                   test_inputs[i][1]),
+#             theta('p', test_inputs[i][2], test_inputs[i][3],
+#                   test_inputs[i][0], test_inputs[i][4],
+#                   test_inputs[i][1]))
 
-        print()
-        print('Input %d:' % i)
-        print('t_annualised: %.5f years' % test_inputs[i][0])
-        print('volatility: %.5f%%' % (100 * test_inputs[i][1]))
-        print('spot price: $%.5f' % test_inputs[i][2])
-        print('strike price: $%.5f' % test_inputs[i][3])
-        print('interest rate: %.5f%%' % (100 * test_inputs[i][4]))
-        print()
-        print('Result %d:' % i)
-        print('Computed call theta: $%0.5f, Expected call theta: $%0.5f' % (
-            got_call, exp_call))
-        print('Computed put theta: $%0.5f, Expected put theta: $%0.5f' % (
-            got_put, exp_put))
+#         print()
+#         print('Input %d:' % i)
+#         print('t_annualised: %.5f years' % test_inputs[i][0])
+#         print('volatility: %.5f%%' % (100 * test_inputs[i][1]))
+#         print('spot price: $%.5f' % test_inputs[i][2])
+#         print('strike price: $%.5f' % test_inputs[i][3])
+#         print('interest rate: %.5f%%' % (100 * test_inputs[i][4]))
+#         print()
+#         print('Result %d:' % i)
+#         print('Computed call theta: $%0.5f, Expected call theta: $%0.5f' % (
+#             got_call, exp_call))
+#         print('Computed put theta: $%0.5f, Expected put theta: $%0.5f' % (
+#             got_put, exp_put))
 
-        check_price(got_call, exp_call)
-        check_price(got_put, exp_put)
+#         check_price(got_call, exp_call)
+#         check_price(got_put, exp_put)
 
 
 # @ pytest.mark.asyncio
@@ -228,6 +228,62 @@ async def test_randomized_black_scholes_theta():
 #             got_call, exp_call))
 
 #         check_price(got_call, exp_call)
+
+@ pytest.mark.asyncio
+async def test_randomized_black_scholes_gamma():
+    # Create a new Starknet class that simulates the StarkNet system.
+    starknet = await Starknet.empty()
+
+    # Deploy the contract.
+    contract_def = compile_starknet_files(files=[CONTRACT_FILE],
+                                          disable_hint_validation=True)
+    contract = await starknet.deploy(
+        contract_def=contract_def,
+    )
+
+    # Number of random tests to run.
+    ITERATIONS = 10
+
+    # List of float tuple (t_annualised, volatility, spot, strike, rate).
+    test_inputs = []
+
+    # Query the contract for options prices.
+    tasks = []
+    for i in range(ITERATIONS):
+        test_input = get_random_test_input()
+        test_inputs.append(test_input)
+        tasks.append(contract.gamma(
+            t_annualised=get_precise(test_input[0]),
+            volatility=get_precise(test_input[1]),
+            spot=get_precise(test_input[2]),
+            strike=get_precise(test_input[3]),
+            rate=get_precise(test_input[4])).call())
+
+    # Compare call and put prices with the python black scholes library.
+    print()
+    execution_infos = await asyncio.gather(*tasks)
+    for i, execution_info in enumerate(execution_infos):
+        # the library expects it be multiplied by .01
+        (got_call) = ((get_lift(execution_info.result.gamma)/UNIT) * .01)
+
+        (exp_call) = (
+            gamma('c', test_inputs[i][2], test_inputs[i][3],
+                  test_inputs[i][0], test_inputs[i][4],
+                  test_inputs[i][1]))
+
+        print()
+        print('Input %d:' % i)
+        print('t_annualised: %.5f years' % test_inputs[i][0])
+        print('volatility: %.5f%%' % (100 * test_inputs[i][1]))
+        print('spot price: $%.5f' % test_inputs[i][2])
+        print('strike price: $%.5f' % test_inputs[i][3])
+        print('interest rate: %.5f%%' % (100 * test_inputs[i][4]))
+        print()
+        print('Result %d:' % i)
+        print('Computed gamma: $%0.5f, Expected gamma: $%0.5f' % (
+            got_call, exp_call))
+
+        check_price(got_call, exp_call)
 
 
 # @pytest.mark.asyncio
